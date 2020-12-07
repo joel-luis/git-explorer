a624549c4657d9f97d53a82b185812c024bc4637
Joel Luis <joelluis73@gmail.com>
2020-12-07 14:45:12 -0300

    Lista de Repositórios

--- src/Repository/index.tsx
+++ src/Repository/index.tsx
@@ -1,15 +1,66 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
 }
 
 const Repository: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository} </h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub-Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/72585274?s=460&u=00d8532d63251b36aaab5f81fd1c7422603fa031&v=4"
+            alt="GitHub Explorer"
+          />
+          <div>
+            <strong>Rockteseat/unform</strong>
+            <p>Descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="Asdkafjkjdkfa">
+          <div>
+            <strong>Adfldkalfka</strong>
+            <p>fdmafkjdafjkajfka</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
